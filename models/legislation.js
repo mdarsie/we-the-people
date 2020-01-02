ddd6c91e387e96f0a929dfd7485d83a80edca2fd
mdarsie <github@darsie.me>
2020-01-02 10:06:31 -0500

    update models, add data

--- models/legislation.js
+++ models/legislation.js
@@ -3,14 +3,11 @@ const mongoose = require("./connection.js");
 const LegislationSchema = new mongoose.Schema({
   title: String,
   billID: String,
-  support: Boolean,
   level: String,
   body: String,
   committee: String,
   subCommittee: String,
   legislationText: String,
-  pending: Boolean,
-  passed: Boolean
 });
 
 const LegislationCollection = mongoose.model("legislation", LegislationSchema);
